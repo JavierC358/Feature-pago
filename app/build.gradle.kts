1a4eb438449edcac88a3b269ebc7d40dec3c4f88
Javier C <javiercardozo358@gmail.com>
2025-06-05 15:07:25 -0400

    Inicio Feature menu

---
+++ app/build.gradle.kts
@@ -0,0 +1,113 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("org.jetbrains.kotlin.kapt")
+    alias(libs.plugins.androidHilt)
+    alias(libs.plugins.googleServices)
+
+    id ("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.example.mordisko"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.mordisko"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+        val googleClientId: String = checkNotNull(
+            project.findProperty("GOOGLE_WEB_CLIENT_ID") as? String
+        ) {
+            "GOOGLE_WEB_CLIENT_ID no está definido en gradle.properties"
+        }
+        buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"$googleClientId\"")
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    // Prueba
+    implementation(platform("androidx.compose:compose-bom:2024.08.00"))
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    //implementation("com.google.firebase:firebase-auth:22.3.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation(libs.hilt.android)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    kapt(libs.hilt.compiler)
+
+    //implementation("androidx.compose.material3:material3-icons-extended:1.2.1")
+    implementation("androidx.compose.material:material-icons-extended:1.6.1")
+
+    implementation(libs.androidx.activity.ktx)
+
+    //Autenticar Login con Google
+
+    implementation(libs.firebase.auth)
+    implementation(libs.google.auth)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.androidHilt) apply false
+    alias(libs.plugins.googleServices) apply false
+
+    // Dager Hill Inyeccion de Dependencias
+
+   // id("com.google.dagger.hilt.android" ) version "2.41" apply false
+
+
+
+}
