8d010cbbd354140e1b307181558019b228a7f01e
Javier C <javiercardozo358@gmail.com>
2025-07-30 14:22:14 -0400

    Actualizar .gitignore para optimizar VCS

--- app/src/main/java/com/example/mordisko/features/user/home/HomeScreen.kt
+++ app/src/main/java/com/example/mordisko/features/user/home/HomeScreen.kt
@@ -14,8 +14,6 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
